ed5e47971d48aa42dbffa454f607d98585c1a7ae
igor.bondezam <igorbfraca@gmail.com>
2024-12-03 22:07:35 -0300

    Adding Swagger documentation

---
+++ src/main/java/com/igor/bondezam/teste/config/SpringDocConfigurations.java
@@ -0,0 +1,15 @@
+package com.igor.bondezam.teste.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+//    @Bean  -- Adicionar quando tiver autenticação
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .components(new Components()
+//                        .addSecuritySchemes("bearer-key",
+//                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+//    }
+}
